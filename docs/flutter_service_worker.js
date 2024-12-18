27f6c99aed7fc02708d31664653bc0f765051289
dirlenor <thanawat.siriwisitthana@gmail.com>
2024-12-18 21:46:21 +0700

    Add viewport meta tag and fullscreen styles

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0a03b1769bf847f0ffdaf20a129bffb0
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "8c10e34e662f30097f17348259a93b0c",
+"flutter_bootstrap.js": "611f76a603974a12d82535382e29b69b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
